fbb4ec5a532c42dcebf57a1f984ebc84739cdf23
Dereje Ayele <derejeatomsa2@gmail.com>
2023-10-10 12:50:11 -0400

    add the third challenge

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,3 +1,4 @@
+import java.util.Comparator;
 import java.util.List;
 
 public class Solution {
@@ -5,6 +6,8 @@ public class Solution {
         System.out.println(malesOnly(Person.persons()));
 
         names(Person.persons()).forEach(System.out::println);
+
+        sortedByIncomeDesc().forEach(System.out::println);
     }
      static List<String> malesOnly(List<Person> people) {
 //        people= Person.persons();
@@ -22,5 +25,14 @@ public class Solution {
                 .toList();
         return nemes;
      }
+     static List<Person> sortedByIncomeDesc(){
+        List<Person> sortedList = Person.persons()
+                .stream()
+                .sorted(Comparator.comparing(Person::getIncome).reversed())
+//                .sorted(Comparator.comparing(Person::getIncome))
+                .toList();
+        return sortedList;
+     }
+
 
 }
