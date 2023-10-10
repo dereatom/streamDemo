69a7712a167c3e6d30c74d5d6c25b018bf6bacf2
Dereje Ayele <derejeatomsa2@gmail.com>
2023-10-10 12:37:07 -0400

    add second challenge

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -3,6 +3,8 @@ import java.util.List;
 public class Solution {
     public static void main(String[] args) {
         System.out.println(malesOnly(Person.persons()));
+
+        names(Person.persons()).forEach(System.out::println);
     }
      static List<String> malesOnly(List<Person> people) {
 //        people= Person.persons();
@@ -11,5 +13,14 @@ public class Solution {
                 .map(Person::getName)
                 .toList();
         return males;
+
+
      }
+     static  List<String> names(List<Person> people){
+        List<String> nemes = people.stream()
+                .map(Person::getName)
+                .toList();
+        return nemes;
+     }
+
 }
